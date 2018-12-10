4444723bb56dcd700e479e695dcf9aa42125d3e0
Kevin <Kevin@DESKTOP-12A507O>
2018-12-10 20:26:55 +0000

    Comments added to the update servlet

--- WebClient/src/ie/gmit/sw/ds/UpdateServlet.java
+++ WebClient/src/ie/gmit/sw/ds/UpdateServlet.java
@@ -56,9 +56,14 @@ public class UpdateServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    // Marshalling and unmarshalling in the below methods were adapted from the file(JAXBPOExample)
+ 	// Available at https://learnonline.gmit.ie/course/view.php?id=590
     
+    // THis method is used to populate data fron an existing order to the form for updating
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//The url to the resource
 		String resourceBaseURL = "http://localhost:8080/DS_Project/webapi/orders/";
+		//The parameter passed to be added to the end of the above url
 		String requestedOrder = request.getParameter("orderNumber");
 		URL url;
 		HttpURLConnection con;
@@ -66,22 +71,31 @@ public class UpdateServlet extends HttpServlet {
 
 		// try to create a connection and request XML format
 		try {
-
+			//The full URL for the resource of the web service
 			url = new URL(resourceBaseURL + requestedOrder);
+			// Open a connection to the resource
 			con = (HttpURLConnection) url.openConnection();
+			// The http method that will be used in the resource.
 			con.setRequestMethod("GET");
+			//Set the request type
 			con.setRequestProperty("Accept", "application/xml");
+			// Get the data input stream
 			InputStream in = con.getInputStream();
+			// Buffered reader to read the xml
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			//Store the xml in the global variable resultInXml as a continious string
 			resultInXml = br.lines().collect(Collectors.joining());
+			// Get a new instance of the ObjectFactory
 			JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+			// Unmarshall the xml data
 			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
+			// read the xml data
 			StringReader reader = new StringReader(resultInXml);
+			//CAst the data to a new carorder
 			CarOrder requested = (CarOrder) unmarshaller.unmarshal(reader);
-
+			// Set attributes for access in the jsp page or on the web service
 			request.setAttribute("requested", requested);
-
+			// Set the forward to update booking page once the request has been completed.
 			String nextJSP = "/UpdateBooking.jsp";
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
 			dispatcher.forward(request, response);
@@ -101,37 +115,52 @@ public class UpdateServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
-	
+	// For updating a order first to the web service then to the database
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// System.out.println(request.getParameter("name"));
+		//PAram list is used to hold all of the values that have been passed through the url
 				ArrayList<String> paramList = new ArrayList<>();
+				// Buffered reader needed to rean the url parameters.
 				BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
-
+				// Store the url in a continuous string
 				String data = br.readLine();
+				// Close the buffered reader.
 				br.close();
 				//This is the only way I could figure out how to get multiple from inputs to variables.
+				// Adapted from : https://stackoverflow.com/questions/855835/servlet-parameters-and-doput
+				// Split the string at '&' and store it in an array
 				String[] params = data.split("&");
+				// Create a new map for the data to be stored
 				Map<String, String> map = new HashMap<String, String>();
+				// Loop over the params array ansd split again at '=' sign
 				for (String param : params) {
 					String name = param.split("=")[0];
 					String value = param.split("=")[1];
+					//put the values into the map
 					map.put(name, value);
+					// Add the value to the array
 					paramList.add(value);
-System.out.println(paramList);
+					//For testing
+					//System.out.println(paramList);
 				}
 				//String.relace is used to remove the + sign from the put request parameters
 				//Adapted from: https://stackoverflow.com/questions/18717557/remove-plus-sign-in-url-query-string
+				
+				// Each of the parameters are noe in order and can be accessed by their position in the array
 				String orderNumber = paramList.get(0);
+				// Strin greplace is suse dto remove the '+' sign that is placed by the url for spaces in a string from a form
 				String name = paramList.get(1).replace("+", " ");
 				String date = paramList.get(2);
 				String country = paramList.get(3).replace("+", " ");
 				String street = paramList.get(4).replace("+", " ");
 				String city = paramList.get(5).replace("+", " ");
 				String model = paramList.get(6).replace("+", " ");
+				// Multiply the cost of the car by the amount of cars ordered to get the final price
 				BigDecimal price = new BigDecimal(paramList.get(7));
-			BigDecimal quantityBD = new BigDecimal(paramList.get(8));
+				BigDecimal quantityBD = new BigDecimal(paramList.get(8));
 				int quantity = quantityBD.intValue();
 				BigDecimal finalPrice = price.multiply(quantityBD);
+				// The url to the web service
 				String resourceBaseURL = "http://localhost:8080/DS_Project/webapi/orders/";
 				
 				URL url;
@@ -140,7 +169,7 @@ System.out.println(paramList);
 
 				// try to create a connection and request XML format
 				try {
-
+					// Set all of the values for the updated order
 					ObjectFactory objFactory = new ObjectFactory();
 
 					CarOrder carOrder = objFactory.createCarOrder();
@@ -162,24 +191,29 @@ System.out.println(paramList);
 					col.add(i1);
 					carOrder.setCars(cars);
 					carOrder.setOrderDate(date);
-					
+					// The full url to the web service
 					url = new URL(resourceBaseURL + orderNumber);
-					
+					// Open a new connection
 					con = (HttpURLConnection) url.openConnection();
 					System.out.println(url);
-
+					// Needed fror put 
 					con.setDoOutput(true);
 					con.setInstanceFollowRedirects(false);
+					// set the request method
 					con.setRequestMethod("PUT");
+					// Xml payload
 					con.setRequestProperty("Content-Type", "application/xml");
 					JAXBContext jc = JAXBContext.newInstance(CarOrder.class);
+					// Marshal the data
 					Marshaller m1 = jc.createMarshaller();
 
 					m1.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+					// Marshal the object to the xnl file
 					m1.marshal(carOrder, new FileWriter("test.xml"));
 					// m1.marshal(carOrder, System.out);
 					OutputStream os = con.getOutputStream();
-
+					//Transformer factory use to read the xml file
+					// Adpated from: https://stackoverflow.com/questions/20143405/how-to-read-a-big-xml-file-in-java-and-spilt-it-into-small-xml-files-based-on-ta
 					TransformerFactory tf = TransformerFactory.newInstance();
 					Transformer transformer = tf.newTransformer();
 					FileReader fileReader = new FileReader("test.xml");
