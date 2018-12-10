81598be6b16a8ace324fdefffe5e0af5eb04e81b
Kevin <Kevin@DESKTOP-12A507O>
2018-12-10 20:06:34 +0000

    More comments added

--- WebClient/src/ie/gmit/sw/ds/CarServlet.java
+++ WebClient/src/ie/gmit/sw/ds/CarServlet.java
@@ -76,9 +76,13 @@ public class CarServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	// Marshalling and unmarshalling in the below methods were adapted from the file(JAXBPOExample)
+	// Available at https://learnonline.gmit.ie/course/view.php?id=590
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		//The url to the resource
 		String resourceBaseURL = "http://localhost:8080/DS_Project/webapi/orders/";
+		//The parameter passed to be added to the end of the above url
 		String requestedOrder = request.getParameter("param1");
 		URL url;
 		HttpURLConnection con;
@@ -86,25 +90,36 @@ public class CarServlet extends HttpServlet {
 
 		// try to create a connection and request XML format
 		try {
-
+			//The full URL for the resource
 			url = new URL(resourceBaseURL + requestedOrder);
+			// Open a connection to the resource
 			con = (HttpURLConnection) url.openConnection();
+			// The http method that will be used in the resource.
 			con.setRequestMethod("GET");
+			// Set the request type
 			con.setRequestProperty("Accept", "application/xml");
+			// Get the data input stream
 			InputStream in = con.getInputStream();
+			// Buffered reader to read the xml
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			//Store the xml in the global variable resultInXml as a continious string
 			resultInXml = br.lines().collect(Collectors.joining());
+			// Get a new instance of the carOrder
 			JAXBContext jaxbContext = JAXBContext.newInstance(CarOrder.class);
+			// Unmarshalling the xml
 			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
+			// Read the xml string
 			StringReader reader = new StringReader(resultInXml);
+			// Bind the xml data to the carOrder object
 			CarOrder requested = (CarOrder) unmarshaller.unmarshal(reader);
-
+			// Set the attribute as requeted can be used to access data on the jsp page.
 			request.setAttribute("requested", requested);
-
+			// Used to forward to a new page once the request has been completed
 			String nextJSP = "/ViewBooking.jsp";
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+			//Forward to the new page.
 			dispatcher.forward(request, response);
+			// Close the connection
 			con.disconnect();
 
 		} catch (IOException | JAXBException e) {
@@ -112,7 +127,7 @@ public class CarServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 
-		// print out the results of the requests
+		// print out the results of the requests for testing
 		System.out.println("****** Result in XML *****");
 		System.out.println(resultInXml);
 		System.out.println();
@@ -126,7 +141,9 @@ public class CarServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
 
 		// System.out.println(request.getParameter("name"));
+		// THe url address to the web service resource 
 		String resourceBaseURL = "http://localhost:8080/DS_Project/webapi/orders/";
+		// Get the parameters from the form that has been posted
 		String requestedOrder = request.getParameter("orderNumber");
 		String custName = request.getParameter("name");
 		String date = request.getParameter("date");
@@ -136,8 +153,9 @@ public class CarServlet extends HttpServlet {
 		String model = request.getParameter("carModel");
 		BigDecimal price = new BigDecimal(request.getParameter("price"));
 		BigDecimal quantityBD = new BigDecimal(request.getParameter("quantity"));
-			int quantity = quantityBD.intValue();
-			BigDecimal finalPrice = price.multiply(quantityBD);
+		int quantity = quantityBD.intValue();
+		// Multiply the price by the quantity to get teh final price
+		BigDecimal finalPrice = price.multiply(quantityBD);
 		
 		URL url;
 		HttpURLConnection con;
@@ -147,8 +165,9 @@ public class CarServlet extends HttpServlet {
 		try {
 
 			ObjectFactory objFactory = new ObjectFactory();
-
+			// A new car order to be sent to the post method in the web service
 			CarOrder carOrder = objFactory.createCarOrder();
+			// Set all of the values to the new object
 			carOrder.setOrderNumber(requestedOrder);
 			Address cust = new Address();
 			cust.setName(custName);
@@ -167,23 +186,30 @@ public class CarServlet extends HttpServlet {
 			col.add(i1);
 			carOrder.setCars(cars);
 			carOrder.setOrderDate(date);
-
+			// The full path to the resourse
 			url = new URL(resourceBaseURL + requestedOrder);
+			// Open a new connection to the web service
 			con = (HttpURLConnection) url.openConnection();
 			System.out.println(url);
-
+			// Needed for the post method
 			con.setDoOutput(true);
+			// Used for redirects allow
 			con.setInstanceFollowRedirects(false);
+			//Set he http method type
 			con.setRequestMethod("POST");
+			// Xml payload
 			con.setRequestProperty("Content-Type", "application/xml");
 			JAXBContext jc = JAXBContext.newInstance(CarOrder.class);
+			//Marshall the data to xml
 			Marshaller m1 = jc.createMarshaller();
 
 			m1.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			//write to a new file overwrite if exists
 			m1.marshal(carOrder, new FileWriter("test.xml"));
 			// m1.marshal(car, System.out);
 			OutputStream os = con.getOutputStream();
-
+			//Transformer factory use to read the xml file
+			// Adpated from: https://stackoverflow.com/questions/20143405/how-to-read-a-big-xml-file-in-java-and-spilt-it-into-small-xml-files-based-on-ta
 			TransformerFactory tf = TransformerFactory.newInstance();
 			Transformer transformer = tf.newTransformer();
 			FileReader fileReader = new FileReader("test.xml");
@@ -192,11 +218,15 @@ public class CarServlet extends HttpServlet {
 			transformer.transform(source, result);
 
 			os.flush();
+			// get the response code for the request
 			con.getResponseCode();
+			// Set the sttributes for access on jsp page
 			request.setAttribute("requested", carOrder);
+			// Forward to the view boooking page
 			String nextJSP = "/ViewBooking.jsp";
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
 			dispatcher.forward(request, response);
+			//Close the connection
 			con.disconnect();
 
 		} catch (IOException | TransformerException | JAXBException e) {
@@ -308,10 +338,12 @@ public class CarServlet extends HttpServlet {
 	
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		//Needed for deleting by http
 		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
-
+		// read the url value there is only one value so no need to split the query.
 		String requestedOrder = br.readLine();
 		br.close();
+		//The url to the web service
 		String resourceBaseURL = "http://localhost:8080/DS_Project/webapi/orders/";
 
 		URL url;
@@ -322,25 +354,33 @@ public class CarServlet extends HttpServlet {
 		try {
 
 			ObjectFactory objFactory = new ObjectFactory();
-
+			// Create a new carORder object
 			CarOrder carOrder = objFactory.createCarOrder();
+			// Ste the order number to delete 
 			carOrder.setOrderNumber(requestedOrder);
+			// The full url to the web service
 			url = new URL(resourceBaseURL + requestedOrder);
+			// Open the connection
 			con = (HttpURLConnection) url.openConnection();
+			// For testing
 			System.out.println("DoDelete");
-
+			// Needed for doDelete in servlet
 			con.setDoOutput(true);
 			con.setInstanceFollowRedirects(false);
+			//Set the request method to be accessed in the web service
 			con.setRequestMethod("DELETE");
+			// Xml payload
 			con.setRequestProperty("Content-Type", "application/xml");
 			JAXBContext jc = JAXBContext.newInstance(CarOrder.class);
+			// MArshall the data to xml
 			Marshaller m1 = jc.createMarshaller();
 
 			m1.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 			m1.marshal(carOrder, new FileWriter("test.xml"));
 			// m1.marshal(car, System.out);
 			OutputStream os = con.getOutputStream();
-
+			//Transformer factory use to read the xml file
+			// Adpated from: https://stackoverflow.com/questions/20143405/how-to-read-a-big-xml-file-in-java-and-spilt-it-into-small-xml-files-based-on-ta
 			TransformerFactory tf = TransformerFactory.newInstance();
 			Transformer transformer = tf.newTransformer();
 			FileReader fileReader = new FileReader("test.xml");
